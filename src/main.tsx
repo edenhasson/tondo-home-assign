25b99193bf74bb4d24b22d8eb79cdcacd3c9c2f7
ehasson <eden_hasson_tp@bmc.com>
2024-12-15 12:57:04 +0200

    dummy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename='/tondo-home-assign'>
             <App/>
         </BrowserRouter>
     </StrictMode>,
