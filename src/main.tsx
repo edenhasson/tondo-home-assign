e14ba415d43a0c5b6db4b91bde247ce3385d0fb1
ehasson <eden_hasson_tp@bmc.com>
2024-12-15 13:01:45 +0200

    dummy

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter basename='/tondo-home-assign'>
+        <HashRouter basename='/tondo-home-assign'>
+        {/* <BrowserRouter basename='/tondo-home-assign'> */}
             <App/>
-        </BrowserRouter>
+        {/* </BrowserRouter> */}
+        </HashRouter>
     </StrictMode>,
 )
